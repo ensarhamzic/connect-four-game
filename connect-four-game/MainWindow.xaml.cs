fd21da1134c0bbc359b36cd508356b54d4f34088
Ensar <ensarhamzic01@gmail.com>
2022-03-18 23:14:34 +0100

    added player class, score changing, playing using numbers on keyboard

--- connect-four-game/MainWindow.xaml.cs
+++ connect-four-game/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace connect_four_game
@@ -19,10 +10,12 @@ namespace connect_four_game
     public partial class MainWindow : Window
     {
         private Table table;
+        private Player player1;
+        private Player player2;
         private int filled;
+        private bool game;
         public enum Turn
         {
-            NONE,
             FIRST,
             SECOND
         };
@@ -31,9 +24,14 @@ namespace connect_four_game
         {
             InitializeComponent();
             table = new Table();
-            playerTurn = Turn.NONE;
-            DrawTable();
+            player1 = new Player("Ensar");
+            player2 = new Player("Tarik");
+            FirstPlayer.Text = player1.Name;
+            SecondPlayer.Text = player2.Name;
+            playerTurn = Turn.FIRST;
+            game = false;
             filled = 0;
+            DrawTable();
         }
 
         private void DrawTable()
@@ -42,7 +40,8 @@ namespace connect_four_game
             Grid.SetRow(mainSP, 1);
             mainSP.HorizontalAlignment = HorizontalAlignment.Center;
             mainSP.VerticalAlignment = VerticalAlignment.Center;
-            for(int i = 0; i < 6; i++)
+
+            for (int i = 0; i < 6; i++)
             {
                 StackPanel fieldSP = new StackPanel();
                 fieldSP.Orientation = Orientation.Horizontal;
@@ -65,7 +64,23 @@ namespace connect_four_game
                     e.Stroke = Brushes.Black;
                     e.StrokeThickness = 2;
                     e.MouseLeftButtonDown += Field_MouseLeftButtonDown;
-                    fieldSP.Children.Add(e);
+                    if(i == 0)
+                    {
+                        StackPanel firstRow = new StackPanel();
+                        TextBlock rowNum = new TextBlock();
+                        rowNum.Text = $"{j + 1}";
+                        rowNum.HorizontalAlignment = HorizontalAlignment.Center;
+                        rowNum.VerticalAlignment = VerticalAlignment.Center;
+                        rowNum.FontWeight = FontWeights.Bold;
+                        rowNum.FontSize = 30;
+                        firstRow.Children.Add(rowNum);
+                        firstRow.Children.Add(e);
+                        fieldSP.Children.Add(firstRow);
+                    } else
+                    {
+                        fieldSP.Children.Add(e);
+                    }
+                    
                 }
                 mainSP.Children.Add(fieldSP);
             }
@@ -75,7 +90,7 @@ namespace connect_four_game
 
         private void Field_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (playerTurn == Turn.NONE) return;
+            if (!game) return;
             Ellipse clickedEllipse = sender as Ellipse;
             int row = int.Parse(clickedEllipse.Name[clickedEllipse.Name.Length - 1].ToString());
             for (int i = 5; i >= 0; i--)
@@ -109,11 +124,13 @@ namespace connect_four_game
                     break;
                 }
             }
+            HighlightPlayer();
         }
 
         private void HasWinner()
         {
             int winner = 0;
+            int[,] winnerFields = new int[4, 2];
             for(int i = 5; i >= 0; i--) // Horizontal win
             {
                 if(winner != 0)
@@ -123,32 +140,23 @@ namespace connect_four_game
                     if(table.Fields[i,j].Value == Field.Val.RED && table.Fields[i, j+1].Value == Field.Val.RED && table.Fields[i, j+2].Value == Field.Val.RED && table.Fields[i, j+3].Value == Field.Val.RED)
                     {
                         winner = 1;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i}c{j+1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i}c{j+2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i}c{j+3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
-                        break;
                     } else if(table.Fields[i, j].Value == Field.Val.YELLOW && table.Fields[i, j + 1].Value == Field.Val.YELLOW && table.Fields[i, j + 2].Value == Field.Val.YELLOW && table.Fields[i, j + 3].Value == Field.Val.YELLOW)
                     {
                         winner = 2;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i}c{j + 1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i}c{j + 2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i}c{j + 3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
+                    }
+                    if(winner != 0)
+                    {
+                        winnerFields[0, 0] = i;
+                        winnerFields[0, 1] = j;
+
+                        winnerFields[1, 0] = i;
+                        winnerFields[1, 1] = j + 1;
+
+                        winnerFields[2, 0] = i;
+                        winnerFields[2, 1] = j + 2;
+
+                        winnerFields[3, 0] = i;
+                        winnerFields[3, 1] = j + 3;
                         break;
                     }
                 }
@@ -163,33 +171,24 @@ namespace connect_four_game
                     if (table.Fields[i, j].Value == Field.Val.RED && table.Fields[i-1, j].Value == Field.Val.RED && table.Fields[i-2, j].Value == Field.Val.RED && table.Fields[i-3, j].Value == Field.Val.RED)
                     {
                         winner = 1;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i-1}c{j}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i-2}c{j}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i-3}c{j}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
-                        break;
                     }
                     else if (table.Fields[i, j].Value == Field.Val.YELLOW && table.Fields[i - 1, j].Value == Field.Val.YELLOW && table.Fields[i - 2, j].Value == Field.Val.YELLOW && table.Fields[i - 3, j].Value == Field.Val.YELLOW)
                     {
                         winner = 2;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i - 1}c{j}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i - 2}c{j}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i - 3}c{j}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
+                    }
+                    if (winner != 0)
+                    {
+                        winnerFields[0, 0] = i;
+                        winnerFields[0, 1] = j;
+
+                        winnerFields[1, 0] = i - 1;
+                        winnerFields[1, 1] = j;
+
+                        winnerFields[2, 0] = i - 2;
+                        winnerFields[2, 1] = j;
+
+                        winnerFields[3, 0] = i - 3;
+                        winnerFields[3, 1] = j;
                         break;
                     }
                 }
@@ -204,33 +203,25 @@ namespace connect_four_game
                     if (table.Fields[i, j].Value == Field.Val.RED && table.Fields[i - 1, j + 1].Value == Field.Val.RED && table.Fields[i - 2, j + 2].Value == Field.Val.RED && table.Fields[i - 3, j + 3].Value == Field.Val.RED)
                     {
                         winner = 1;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i - 1}c{j + 1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i - 2}c{j + 2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i - 3}c{j + 3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
-                        break;
                     }
                     else if (table.Fields[i, j].Value == Field.Val.YELLOW && table.Fields[i - 1, j + 1].Value == Field.Val.YELLOW && table.Fields[i - 2, j + 2].Value == Field.Val.YELLOW && table.Fields[i - 3, j + 3].Value == Field.Val.YELLOW)
                     {
                         winner = 2;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i - 1}c{j + 1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i - 2}c{j + 2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i - 3}c{j + 3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
+                    }
+
+                    if (winner != 0)
+                    {
+                        winnerFields[0, 0] = i;
+                        winnerFields[0, 1] = j;
+
+                        winnerFields[1, 0] = i - 1;
+                        winnerFields[1, 1] = j + 1;
+
+                        winnerFields[2, 0] = i - 2;
+                        winnerFields[2, 1] = j + 2;
+
+                        winnerFields[3, 0] = i - 3;
+                        winnerFields[3, 1] = j + 3;
                         break;
                     }
                 }
@@ -245,54 +236,59 @@ namespace connect_four_game
                     if (table.Fields[i, j].Value == Field.Val.RED && table.Fields[i - 1, j - 1].Value == Field.Val.RED && table.Fields[i - 2, j - 2].Value == Field.Val.RED && table.Fields[i - 3, j - 3].Value == Field.Val.RED)
                     {
                         winner = 1;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i - 1}c{j - 1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i - 2}c{j - 2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i - 3}c{j - 3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
-                        break;
                     }
                     else if (table.Fields[i, j].Value == Field.Val.YELLOW && table.Fields[i - 1, j - 1].Value == Field.Val.YELLOW && table.Fields[i - 2, j - 2].Value == Field.Val.YELLOW && table.Fields[i - 3, j - 3].Value == Field.Val.YELLOW)
                     {
                         winner = 2;
-                        this.Dispatcher.Invoke(() =>
-                        {
-                            Ellipse el1 = GameGrid.FindName($"r{i}c{j}") as Ellipse;
-                            Ellipse el2 = GameGrid.FindName($"r{i - 1}c{j - 1}") as Ellipse;
-                            Ellipse el3 = GameGrid.FindName($"r{i - 2}c{j - 2}") as Ellipse;
-                            Ellipse el4 = GameGrid.FindName($"r{i - 3}c{j - 3}") as Ellipse;
-                            el1.StrokeThickness = 15;
-                            el2.StrokeThickness = 15;
-                            el3.StrokeThickness = 15;
-                            el4.StrokeThickness = 15;
-                        });
+                    }
+
+                    if (winner != 0)
+                    {
+                        winnerFields[0, 0] = i;
+                        winnerFields[0, 1] = j;
+
+                        winnerFields[1, 0] = i - 1;
+                        winnerFields[1, 1] = j - 1;
+
+                        winnerFields[2, 0] = i - 2;
+                        winnerFields[2, 1] = j - 2;
+
+                        winnerFields[3, 0] = i - 3;
+                        winnerFields[3, 1] = j - 3;
                         break;
                     }
                 }
             }
 
-
             if (winner != 0)
             {
-                MessageBox.Show($"Winner is player {winner}");
-                playerTurn = Turn.NONE;
+                for(int i = 0; i < 4; i++)
+                {
+                    Ellipse el = GameGrid.FindName($"r{winnerFields[i,0]}c{winnerFields[i, 1]}") as Ellipse;
+                    el.StrokeThickness = 15;
+                }
+                game = false;
+                StartButton.IsEnabled = true;
+                if(winner == 1)
+                {
+                    player1.Score++;
+                } else
+                {
+                    player2.Score++;
+                }
+                Score.Text = $"{player1.Score} : {player2.Score}";
+            } else if(filled == 42)
+            {
+                game = false;
+                StartButton.IsEnabled = true;
             }
-                
 
-            // Winning logic
-            // Increment score
-            // Game end no winner message
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            playerTurn = Turn.FIRST;
+            game = true;
+            StartButton.IsEnabled = false;
             filled = 0;
             for (int i = 0; i < 6; i++)
             {
@@ -306,6 +302,75 @@ namespace connect_four_game
                     });
                     table.Fields[i, j] = new Field(); // Resetting
                 }
+            }
+            HighlightPlayer();
+        }
+
+        private void HighlightPlayer()
+        {
+            if(playerTurn == Turn.FIRST)
+            {
+                FirstPlayer.TextDecorations = TextDecorations.Underline;
+                SecondPlayer.TextDecorations = null;
+            } else
+            {
+                SecondPlayer.TextDecorations = TextDecorations.Underline;
+                FirstPlayer.TextDecorations = null;
+            }
+        }
+
+        
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool turn = false;
+            Ellipse el = new Ellipse();
+            if(e.Key == Key.D1 || e.Key == Key.NumPad1)
+            {
+                el = GameGrid.FindName($"r{0}c{0}") as Ellipse;
+                turn = true;
+                
+            }
+            else if (e.Key == Key.D2 || e.Key == Key.NumPad2)
+            {
+                el = GameGrid.FindName($"r{0}c{1}") as Ellipse;
+                turn = true;
+            }
+            else if (e.Key == Key.D3 || e.Key == Key.NumPad3)
+            {
+                el = GameGrid.FindName($"r{0}c{2}") as Ellipse;
+                turn = true;
+            }
+            else if (e.Key == Key.D4 || e.Key == Key.NumPad4)
+            {
+                el = GameGrid.FindName($"r{0}c{3}") as Ellipse;
+                turn = true;
+            }
+            else if (e.Key == Key.D5 || e.Key == Key.NumPad5)
+            {
+                el = GameGrid.FindName($"r{0}c{4}") as Ellipse;
+                turn = true;
+            }
+            else if (e.Key == Key.D6 || e.Key == Key.NumPad6)
+            {
+                el = GameGrid.FindName($"r{0}c{5}") as Ellipse;
+                turn = true;
+            }
+            else if (e.Key == Key.D7 || e.Key == Key.NumPad7)
+            {
+               el = GameGrid.FindName($"r{0}c{6}") as Ellipse;
+                turn = true;
+            }
+            else if(e.Key == Key.Space && StartButton.IsEnabled)
+            {
+                StartButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
+
+
+            if (turn)
+            {
+                MouseButtonEventArgs arg = new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, MouseButton.Left);
+                arg.RoutedEvent = Ellipse.MouseLeftButtonDownEvent;
+                el.RaiseEvent(arg);
             }
         }
     }
---
+++ connect-four-game/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect_four_game
+{
+    internal class Player
+    {
+        int score;
+        string name;
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Player()
+        {
+            score = 0;
+            name = "Name";
+        }
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
